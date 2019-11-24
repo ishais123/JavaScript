b0f48a8710ac0978873658ec62266ada35f48fc1
Ishai Shahak <ishai@tera-sky.com>
2019-11-24 18:04:36 +0200

    last update

--- substrings.js
+++ substrings.js
@@ -1,17 +1,57 @@
+/*
 function subString (number)
 {
     var count = 0;
     var numstring = number.toString();
     var split1 = numstring.split("");
-  if (number == 0) 
-  {
-      return 0;
-  }
+    var sub = split1.slice(1);
+    if (number == 0) 
+     {
+         return 0;
+     }
+while (number > 0)
+{
   for (var i = 0; i < split1.length; i++)
   {
-    if (number%split1[i] == 0) {count ++}
+    if (split1[i] > 0) {
+        if (number%split1[i] == 0) {
+            count ++
+        }
+    }
   }
+  number = number / (1*split1.length);
+}
   return count
 }
 
 console.log(subString(877692));
+
+
+*/
+function subString (number)
+{
+    var split = number.toString().split("");
+    var count = 0;
+
+    for (var i = 0; i < split.length; i++)
+    {
+        if (split[i] > 0) {
+            if (number%split[i] == 0) {
+                count ++;
+            }
+        }
+    }    
+    for (var i = 0; i < split.length; i++)
+    {
+    var newnum = parseInt(split[i]+split[i+1], 10);
+    if (number%newnum == 0) {
+        count ++;
+    }
+    return count;
+}
+ console.log(subString(877692));
+/*
+console.log(length);
+console.log(split);
+console.log(newnum);
+*/
